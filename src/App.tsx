f6ca0b1fd20dcac14739a547f7518626f7f97051
niksfred <niksfred95@gmail.com>
2022-08-17 10:45:29 +0300

    react + ts + tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-slate-700 w-screen h-screen flex justify-center items-center">
+      <header className="App-header">
+        <h3 className="text-xl text-white">React comments application</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
